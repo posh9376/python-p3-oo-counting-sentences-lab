d8122a38b94bf0c0a20396dfc7cf29bb8ce1a828
Eric Richard <reric0802@gmail.com>
2024-12-10 14:13:33 +0300

    added code to ensure passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self,value=""):
+        if not isinstance(value,str):
+            print("The value must be a string.")
+        self.value = value
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self,value):
+        if not isinstance(value,str):
+            print("The value must be a string.")
+        self._value = value
+    def is_sentence(self): 
+        return self.value.endswith('.')
+    def is_question(self):
+        return self.value.endswith('?')
+    def is_exclamation(self):
+        return self.value.endswith('!')
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]+', self.value)
+        return len([s for s in sentences if s.strip()])
